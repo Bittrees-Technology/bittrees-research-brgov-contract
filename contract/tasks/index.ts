f6f5d211e338094920abbe3ffc08c2135e812dda
danielvdg <daniel.vdghinste@valr.com>
2025-05-18 20:21:54 +0200

    chore: update index.ts

--- contract/tasks/index.ts
+++ contract/tasks/index.ts
@@ -1,10 +1,11 @@
 import './compositions';
 import './ERC20';
 import './grant-role-tasks';
-import './renounce-role-tasks';
-import './revoke-role-tasks';
-import './set-payment-token-tasks';
-import './set-treasury-tasks';
 import './mint-batch-tasks';
 import './pause-tasks';
+import './renounce-role-tasks';
+import './revoke-role-tasks';
+import './set-base-uri-tasks';
+import './set-payment-token-tasks';
+import './set-treasury-tasks';
 import './unpause-tasks';
